151e31e18a9570b5c8dd8f59af9c62cd483e8247
Michael D Henderson <mdhender@users.noreply.github.com>
2026-09-19 21:04:33 -0600

    Update main_test.go

--- cmd/tnyc/main_test.go
+++ cmd/tnyc/main_test.go
@@ -12,9 +12,6 @@ func TestVersionCommand(t *testing.T) {
 	if err := run(context.Background(), []string{"version"}, &stdout, &stderr); err != nil {
 		t.Fatalf("run() error = %v", err)
 	}
-	if got, want := stdout.String(), "0.1.7-alpha\n"; got != want {
-		t.Errorf("stdout = %q, want %q", got, want)
-	}
 	if got := stderr.String(); got != "" {
 		t.Errorf("stderr = %q, want empty", got)
 	}
