94480a48957141d1066eb9f7ac0a34bdcfe349eb
Michael D Henderson <mdhender@users.noreply.github.com>
2026-09-19 21:01:19 -0600

    Delete version_test.go

--- version_test.go
+++
@@ -1,9 +0,0 @@
-package tnyc
-
-import "testing"
-
-func TestVersion(t *testing.T) {
-	if got, want := Version().String(), "0.1.7-alpha"; got != want {
-		t.Fatalf("Version().String() = %q, want %q", got, want)
-	}
-}
